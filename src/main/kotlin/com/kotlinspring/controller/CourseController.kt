67895c488928d4f68da41aaeb562e941d9a93556
Luke Guan <lukeguan@outlook.com>
2022-04-19 23:14:13 -0700

    put method

--- src/main/kotlin/com/kotlinspring/controller/CourseController.kt
+++ src/main/kotlin/com/kotlinspring/controller/CourseController.kt
@@ -3,12 +3,7 @@ package com.kotlinspring.controller
 import com.kotlinspring.dto.CourseDTO
 import com.kotlinspring.service.CourseService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 
 @RestController
@@ -22,4 +17,7 @@ class CourseController(val courseService: CourseService) {
 
     @GetMapping
     fun retrieveAllCourses(): List<CourseDTO> = courseService.retrieveAllCourses()
+
+    @PutMapping("/{course_id}")
+    fun updateCourse(@RequestBody courseDTO: CourseDTO, @PathVariable("course_id") courseId: Int) = courseService.updateCourse(courseId, courseDTO)
 }
